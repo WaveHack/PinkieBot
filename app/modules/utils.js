5d8fbcc94924fb92544f2c1665af83dd8ef1ae5a
WaveHack <email@wavehack.net>
2016-04-07 00:20:01 +0200

    Same for utils. Also added temp eval command for testing

--- app/modules/utils.js
+++ app/modules/utils.js
@@ -2,8 +2,8 @@
 
 var utils = {
     meta: {
-        id: 'core',
-        name: 'Core',
+        id: 'utils',
+        name: 'Utils Module',
         description: 'PinkieBot core module'
     },
     dependencies: [],
@@ -30,11 +30,20 @@ utils.init = function (modsys) {
 
     modsys.addCommand(this, 'serverid', function (client, message, args) {
         // todo: only in non-pm
-        global.helpers.reply(client, message, message.channel.server.id);
+        global.helpers.reply(client, message, ('Server ID: ' + message.channel.server.id));
     });
 
     modsys.addCommand(this, 'channelid', function (client, message, args) {
-        global.helpers.reply(client, message, message.channel.id);
+        global.helpers.reply(client, message, ('Channel ID: ' + message.channel.id));
+    });
+
+    modsys.addCommand(this, 'eval', function (client, message, args) {
+       try {
+           var r = eval(args.join(''));
+           global.helpers.reply(client, message, r);
+       } catch (e) {
+           global.helpers.reply(client, message, ('Error: ' + e));
+       }
     });
 
     return true;
